f420cde72bd1b3b1c42a68e7456276c7c23b51a3
ChunHLi <chunhli@cs.stonybrook.edu>
2018-10-27 18:47:57 -0700

    getting the hang of it

---
+++ bool.h
@@ -0,0 +1,9 @@
+/*	bool.h
+	simple boolean datatype
+*/
+
+#define TRUE    1
+#define FALSE   0
+
+typedef int bool;
+
---
+++ min-bandwidth.c
@@ -0,0 +1,14 @@
+#include "bool.h"
+#include "backtrack.h"
+
+process_solution(int a[], int k)
+{
+}
+
+is_a_solution(int a[], int n)
+{
+}
+
+construct_candidates(int a[], int k, int n, int c[], int *ncandidates)
+{
+}
